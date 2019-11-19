a7eee5773c9894a9d8d5aeef6bb7ba80f1371444
DragonBanana <10493858@Polimi.it>
2019-11-19 19:19:39 +0100

    Starting project
    Starting project

---
+++ run_all_algorithms.py
@@ -0,0 +1,85 @@
+
+from SLIM_BPR.Cython.SLIM_BPR_Cython import SLIM_BPR_Cython
+from SLIM_ElasticNet.SLIMElasticNetRecommender import SLIMElasticNetRecommender
+
+from MatrixFactorization.Cython.MatrixFactorization_Cython import MatrixFactorization_BPR_Cython, MatrixFactorization_FunkSVD_Cython, MatrixFactorization_AsySVD_Cython
+from MatrixFactorization.PureSVDRecommender import PureSVDRecommender
+from MatrixFactorization.WRMFRecommender import WRMFRecommender
+
+from Base.NonPersonalizedRecommender import TopPop, Random, GlobalEffects
+
+from KNN.UserKNNCFRecommender import UserKNNCFRecommender
+from KNN.ItemKNNCFRecommender import ItemKNNCFRecommender
+from KNN.ItemKNNCBFRecommender import ItemKNNCBFRecommender
+from GraphBased.RP3betaRecommender import RP3betaRecommender
+from GraphBased.P3alphaRecommender import P3alphaRecommender
+
+from Data_manager.Movielens10M.Movielens10MReader import Movielens10MReader
+from Data_manager.DataSplitter_leave_k_out import DataSplitter_leave_k_out
+
+import traceback, os
+
+
+if __name__ == '__main__':
+
+
+    dataset_object = Movielens10MReader()
+
+    dataSplitter = DataSplitter_leave_k_out(dataset_object, k_value=2)
+
+    dataSplitter.load_data()
+    URM_train, URM_validation, URM_test = dataSplitter.get_holdout_split()
+
+    recommender_list = [
+        Random,
+        TopPop,
+        GlobalEffects,
+        UserKNNCFRecommender,
+        ItemKNNCFRecommender,
+        P3alphaRecommender,
+        RP3betaRecommender,
+        SLIM_BPR_Cython,
+        SLIMElasticNetRecommender,
+        MatrixFactorization_BPR_Cython,
+        MatrixFactorization_FunkSVD_Cython,
+        MatrixFactorization_AsySVD_Cython,
+        PureSVDRecommender,
+        WRMFRecommender,
+    ]
+
+
+    from Base.Evaluation.Evaluator import EvaluatorHoldout
+
+    evaluator = EvaluatorHoldout(URM_test, [5, 20], exclude_seen=True)
+
+
+    output_root_path = "./result_experiments/"
+
+    # If directory does not exist, create
+    if not os.path.exists(output_root_path):
+        os.makedirs(output_root_path)
+
+
+    logFile = open(output_root_path + "result_all_algorithms.txt", "a")
+
+
+    for recommender_class in recommender_list:
+
+        try:
+
+            print("Algorithm: {}".format(recommender_class))
+
+
+            recommender = recommender_class(URM_train)
+            recommender.fit()
+
+            results_run, results_run_string = evaluator.evaluateRecommender(recommender)
+
+            print("Algorithm: {}, results: \n{}".format(recommender.__class__, results_run_string))
+            logFile.write("Algorithm: {}, results: \n{}\n".format(recommender.__class__, results_run_string))
+            logFile.flush()
+
+        except Exception as e:
+            traceback.print_exc()
+            logFile.write("Algorithm: {} - Exception: {}\n".format(recommender_class, str(e)))
+            logFile.flush()
---
+++ run_parameter_search.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 22/11/17
+
+@author: Maurizio Ferrari Dacrema
+"""
+
+from Base.NonPersonalizedRecommender import TopPop, Random
+from KNN.UserKNNCFRecommender import UserKNNCFRecommender
+from KNN.ItemKNNCFRecommender import ItemKNNCFRecommender
+from SLIM_BPR.Cython.SLIM_BPR_Cython import SLIM_BPR_Cython
+from SLIM_ElasticNet.SLIMElasticNetRecommender import SLIMElasticNetRecommender
+from GraphBased.P3alphaRecommender import P3alphaRecommender
+from GraphBased.RP3betaRecommender import RP3betaRecommender
+from MatrixFactorization.Cython.MatrixFactorization_Cython import MatrixFactorization_BPR_Cython, MatrixFactorization_FunkSVD_Cython
+from MatrixFactorization.PureSVDRecommender import PureSVDRecommender
+
+import traceback
+
+import os, multiprocessing
+from functools import partial
+
+
+
+from data.Movielens_10M.Movielens10MReader import Movielens10MReader
+from ParameterTuning.run_parameter_search import runParameterSearch_Collaborative
+
+
+def read_data_split_and_search():
+    """
+    This function provides a simple example on how to tune parameters of a given algorithm
+
+    The BayesianSearch object will save:
+        - A .txt file with all the cases explored and the recommendation quality
+        - A _best_model file which contains the trained model and can be loaded with recommender.load_model()
+        - A _best_parameter file which contains a dictionary with all the fit parameters, it can be passed to recommender.fit(**_best_parameter)
+        - A _best_result_validation file which contains a dictionary with the results of the best solution on the validation
+        - A _best_result_test file which contains a dictionary with the results, on the test set, of the best solution chosen using the validation set
+    """
+
+
+
+    dataReader = Movielens10MReader()
+
+    URM_train = dataReader.get_URM_train()
+    URM_validation = dataReader.get_URM_validation()
+    URM_test = dataReader.get_URM_test()
+
+    output_folder_path = "result_experiments/"
+
+
+    # If directory does not exist, create
+    if not os.path.exists(output_folder_path):
+        os.makedirs(output_folder_path)
+
+
+
+
+
+
+
+    collaborative_algorithm_list = [
+        Random,
+        TopPop,
+        P3alphaRecommender,
+        RP3betaRecommender,
+        ItemKNNCFRecommender,
+        UserKNNCFRecommender,
+        MatrixFactorization_BPR_Cython,
+        MatrixFactorization_FunkSVD_Cython,
+        PureSVDRecommender,
+        SLIM_BPR_Cython,
+        SLIMElasticNetRecommender
+    ]
+
+
+
+
+    from Base.Evaluation.Evaluator import EvaluatorHoldout
+
+    evaluator_validation = EvaluatorHoldout(URM_validation, cutoff_list=[5])
+    evaluator_test = EvaluatorHoldout(URM_test, cutoff_list=[5, 10])
+
+
+    runParameterSearch_Collaborative_partial = partial(runParameterSearch_Collaborative,
+                                                       URM_train = URM_train,
+                                                       metric_to_optimize = "MAP",
+                                                       n_cases = 10,
+                                                       evaluator_validation_earlystopping = evaluator_validation,
+                                                       evaluator_validation = evaluator_validation,
+                                                       evaluator_test = evaluator_test,
+                                                       output_folder_path = output_folder_path,
+                                                       similarity_type_list = ["cosine"],
+                                                       parallelizeKNN = False)
+
+
+
+
+
+    pool = multiprocessing.Pool(processes=int(multiprocessing.cpu_count()), maxtasksperchild=1)
+    pool.map(runParameterSearch_Collaborative_partial, collaborative_algorithm_list)
+
+    #
+    #
+    # for recommender_class in collaborative_algorithm_list:
+    #
+    #     try:
+    #
+    #         runParameterSearch_Collaborative_partial(recommender_class)
+    #
+    #     except Exception as e:
+    #
+    #         print("On recommender {} Exception {}".format(recommender_class, str(e)))
+    #         traceback.print_exc()
+    #
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+
+    read_data_split_and_search()
