ed2888a8bb0ceb7f0ebbf1ca26fff9767c376033
DragonBanana <10493858@Polimi.it>
2019-11-25 18:50:55 +0100

    Fixing log_to_file_evaluator
    Fixing log_to_file_evaluator

--- Base/Evaluation/LogToFileEvaluator.py
+++ Base/Evaluation/LogToFileEvaluator.py
@@ -7,9 +7,9 @@ def evaluate(data, random_seed_used_in_data, rec, rec_name, rec_args, filename="
     for n, users, description in data.urm_train_users_by_type:
         eval, map = MyEvaluator.evaluate_algorithm_parallel(data.urm_test, users, rec, at=10, remove_top=0,
                                                             parallelism=2)
-        f.write(f"{random_seed_used_in_data}, {rec_name}, {rec_args}, {description}, {map}")
+        f.write(f"{random_seed_used_in_data}, {rec_name}, {rec_args}, {description}, {map}\n")
         f.flush()
     eval, map = MyEvaluator.evaluate_algorithm(data.urm_test, data.ids_target_users, rec, at=10,
                                                remove_top=0)
-    f.write(f"{random_seed_used_in_data}, {rec_name}, {rec_args}, target_users, {map}")
+    f.write(f"{random_seed_used_in_data}, {rec_name}, {rec_args}, target_users, {map}\n")
     f.flush()
